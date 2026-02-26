75dda2f79047228e9926788abe77f91e4c5c16e3
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2026-02-26 21:43:02 +0800

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,11 @@ export default function Home() {
 
   const projects = [
     {
+      title: "Physics-Guided AI For Enhanced Heat Index Forecasting in the Philippine Context",
+      year: "2026",
+      description: "Developed as a Data Engineer, this project involves collecting datasets via web scraping from X, Facebook LGU pages, and PAGASA official sources for machine learning and data analysis to improve heat index forecasting accuracy using physics-guided AI approaches."
+    },
+    {
       title: "Data-Driven Analysis of Daily Stress Levels and the Effectiveness of Stress-Relief Activities",
       year: "2026",
       description: "The project focuses on analyzing personal stress data collected from a smartwatch to understand daily stress patterns and evaluate the effectiveness of various stress-relief activities."
